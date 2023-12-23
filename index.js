679571ab9be878f7bee9db4c53c974b23992b89a
kent riggs <k2r3@proton.me>
2023-12-23 14:31:06 -0700

    added javascript to solve for tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left.replace("px", ""));
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left.replace("px", ""));
+  const rightEdge = window.innerWidth - 40; // Assuming the dodger is 40px wide
+  if (left < rightEdge) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
